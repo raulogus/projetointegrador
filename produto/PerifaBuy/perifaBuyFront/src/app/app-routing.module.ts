9942bb3bc94d14748af54d923ff5cc6abbf804ff
Larissaribeiro03 <larissaribeiro03@hotmail.com>
2021-01-21 15:39:25 -0300

    Implementação de componentes

---
+++ produto/PerifaBuy/perifaBuyFront/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+const routes: Routes = [
+
+{path:'', redirectTo: "PAGINA HOME A SER FEITA" },
+{path:'entrar', component: EntrarComponent},
+{path:'cadastrar', component: CadastrarComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
