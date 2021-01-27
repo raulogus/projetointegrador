db6dc6db841904e0c965e48815bc829f1ddd0e15
Ricardo Magalhães <ricardomrfin@gmail.com>
2021-01-26 21:25:32 -0300

    6 models criadas no front end

---
+++ produto/Front end/perifaBuyFront/src/app/model/carrinho.ts
@@ -0,0 +1,8 @@
+export class carrinho{
+    public id:number
+    public id_produto:number
+    public id_cliente:number
+    public valorTotal:number
+    public formaPagamento:string
+    public data:Date
+}
---
+++ produto/Front end/perifaBuyFront/src/app/model/cliente.ts
@@ -0,0 +1,10 @@
+export class cliente{
+    public id: number
+    public username:string
+    public nome:string
+    public data:Date
+    public genero:string
+    public email:string
+    public password:string
+    public localizacao:string
+}
